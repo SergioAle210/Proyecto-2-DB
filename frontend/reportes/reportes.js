5eb8af4459cbaf4f9d1cc848a6071915b3a7570e
Andre Marroquin <mar22266@uvg.edu.gt>
2024-04-13 19:19:59 -0600

    added front files

--- frontend/reportes/reportes.js
+++ frontend/reportes/reportes.js
@@ -1,22 +1,20 @@
 function obtenerReporteVentas() {
-// reportes.js
 
-// Función para solicitar un reporte de ventas desde el servidor
 function obtenerReporteVentas() {
     fetch('http://localhost:3000/api/reportes/ventas', {
-        method: 'GET', // o 'POST' si así se requiere en tu API
+        method: 'GET', 
         headers: {
             'Content-Type': 'application/json',
-            // Incluye aquí otros encabezados si son necesarios, como tokens de autenticación
+            
         },
-        // Si necesitas enviar un cuerpo de solicitud, como fechas de filtro, inclúyelo aquí
+        
     })
     .then(response => {
-        // Verificamos primero si la respuesta del servidor es exitosa
+  
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        // Luego verificamos que la respuesta sea de tipo JSON
+ 
         const contentType = response.headers.get('content-type');
         if (!contentType || !contentType.includes('application/json')) {
             throw new Error('No se recibió una respuesta JSON válida');
@@ -24,19 +22,19 @@ function obtenerReporteVentas() {
         return response.json();
     })
     .then(data => {
-        // Aquí procesas y muestras los datos del reporte
+     
         console.log(data);
         const reporteVentasDiv = document.getElementById('reporteVentas');
         reporteVentasDiv.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
     })
     .catch(error => {
-        // Aquí manejas cualquier error que ocurra durante la solicitud o el procesamiento de la respuesta
+        
         console.error('Error al obtener el reporte de ventas:', error);
         alert('Error al obtener el reporte de ventas: ' + error.message);
     });
 }
 
-// Llamamos a la función cuando se hace clic en el botón de 'Generar Reporte'
+
 document.addEventListener('DOMContentLoaded', () => {
     const botonReporteVentas = document.querySelector('#reporteVentas button');
     botonReporteVentas.addEventListener('click', obtenerReporteVentas);
