360f91405af43a224ecdbaaefa2b6e8091c45038
SergioAle210 <98841969+SergioAle210@users.noreply.github.com>
2024-04-14 23:57:48 -0600

    feat: Added all necessary documents for bar view

---
+++ frontend/bar/bar.js
@@ -0,0 +1,24 @@
+function obtenerListadoBebidas() {
+    fetch(`http://localhost:3000/api/bar/bebidas-a-preparar`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const bebidasListDiv = document.getElementById('bebidasList');
+            bebidasListDiv.innerHTML = ''; // Limpiar el contenido anterior
+            data.forEach(bebida => {
+                bebidasListDiv.innerHTML += `<p>Fecha: ${bebida.fecha_pedido} - Hora: ${bebida.hora_pedido}:${bebida.minutos_pedido} - Bebida: ${bebida.nombre_bebida}</p>`;
+            });
+        })
+        .catch(error => {
+            console.error('Error al obtener el listado de bebidas:', error);
+            alert('Error al obtener el listado de bebidas: ' + error.message);
+        });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    obtenerListadoBebidas();
+});
---
+++ server/src/rutas/Bar.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../../conn'); 
+
+router.get('/bebidas-a-preparar', async (req, res) => {   
+    try {
+        const result = await pool.query(
+            `SELECT 
+                i.Nombre AS nombre_bebida,
+                TO_CHAR(p.Fecha_Hora_Pedido, 'YYYY-MM-DD') AS fecha_pedido,
+                EXTRACT(HOUR FROM p.Fecha_Hora_Pedido) AS hora_pedido,
+                EXTRACT(MINUTE FROM p.Fecha_Hora_Pedido) AS minutos_pedido
+            FROM 
+                Pedidos p
+            JOIN 
+                Detalle_Pedido dp ON p.ID_Pedido = dp.ID_Pedido
+            JOIN 
+                Items i ON dp.ID_Item = i.ID_Item
+            WHERE 
+                i.Tipo = 'bebida'
+                AND p.Estado IS NULL -- Solo seleccionar pedidos no completados
+            ORDER BY 
+                p.Fecha_Hora_Pedido;`
+        );
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al obtener el listado de bebidas a preparar');
+    }
+});
+
+module.exports = router;
